14576aa5f81d2319917c81efa409e22735d4c574
nero-dante <zhongww3@uci.edu>
2018-06-14 14:37:03 -0700

    add comment

--- TaxiDataDHT/src/Conn/DHTable.java
+++ TaxiDataDHT/src/Conn/DHTable.java
@@ -49,11 +49,6 @@ public class DHTable {
 		String tempstr = null;
 		while((tempstr = br.readLine()) != null) {
 			JSONObject tempjs = new JSONObject(tempstr);
-			if(tempjs.getInt("count") < 100) {
-				int tempint = tempjs.getInt("count")*100;
-				tempjs.remove("count");
-				tempjs.put("count", tempint);
-			}
 			tempjs.put("coordinates", getCoordinate(tempjs.getInt("prediction")));
 			jarr.put(tempjs);
 		}
@@ -65,34 +60,33 @@ public class DHTable {
 	}
 	static public void main(String[] args) throws NamingException, IOException, InterruptedException {
 		
-		String url = "rmi://54.202.179.219:1099/";
+		String url = "rmi://54.202.179.219:1099/";//set to destination url
 		Context ncontext = new InitialContext();
 		DHTService ser = (DHTService)ncontext.lookup(url+"DHTService");
 		System.out.println("client created");
 		String filePath = "/home/ubuntu/TaxiDataAnalysis/spark/";
 		System.out.println("success");
-		ser.sendMessage("Manhattan", "hahaha");
-//		DHTable dt = new DHTable(args[1]);
-//		int month_counter = 4;
-//		int tag = 1;
-//		switch(args[1]) {
-//		case "Manhattan":
-//			tag = 1; break;
-//		case "Queen":
-//			tag =4; break;
-//		case "The Bronx":
-//			tag = 2; break;
-//		case "Brooklyn":
-//			tag = 3; break;
-//		}
-//		while(true) {
-//			//build the data path
-//			dt.couPath = filePath+"KmResult"+tag+month_counter+"/"+"km"+tag+month_counter+".json";
-//			dt.loPath = dt.couPath = filePath+"bz"+tag+month_counter+"/"+"bz"+tag+month_counter+".json";
-//			//
-//			ser.sendMessage(dt.DType, dt.parseData());
-//			//parseData after every 100 seconds
-//			Thread.sleep(10000);
-//		}
+		DHTable dt = new DHTable(args[1]);
+		int month_counter = 4;
+		int tag = 1;
+		switch(args[1]) {
+		case "Manhattan":
+			tag = 1; break;
+		case "Queen":
+			tag =4; break;
+		case "The Bronx":
+			tag = 2; break;
+		case "Brooklyn":
+			tag = 3; break;
+		}
+		while(true) {
+			//build the data path
+			dt.couPath = filePath+"KmResult"+tag+month_counter+"/"+"km"+tag+month_counter+".json";
+			dt.loPath = dt.couPath = filePath+"bz"+tag+month_counter+"/"+"bz"+tag+month_counter+".json";
+			//
+			ser.sendMessage(dt.DType, dt.parseData());
+			//parseData after every 100 seconds
+			Thread.sleep(10000);
+		}
 	}
 }
